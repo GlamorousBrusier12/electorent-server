099e2ca4393b9d3f4295f37f2fb8b9995c2facd1
Naveen <pandurebelsachin18@gmail.com>
2022-03-28 23:32:18 +0530

    added delete order

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,13 @@
 import express from "express";
-import { createUser, getUser, updateUser } from "../controllers/user.js";
+import {
+  createUser,
+  deleteUser,
+  getUser,
+  updateUser,
+} from "../controllers/user.js";
 export const userRouter = express.Router();
 
 // test route for api
 userRouter.post("/", createUser);
-userRouter.route("/:uid").get(getUser).patch(updateUser);
+userRouter.route("/:uid").get(getUser);
+userRouter.route("/").patch(updateUser).delete(deleteUser);
