a26eb5d2b655a1f2ae0b93acf2f87035c4f1b02d
Ravindra Tholuchuru <tholuchururavindra@gmail.com>
2022-04-03 23:51:03 +0530

    Added JWT

--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -5,9 +5,14 @@ export const loginRoute = express.Router();
 import { SECRET_KEY } from "../config/config.js";
 import { User } from "../models/User.js";
 import { userRouter } from "./user.js";
+
+function saveTokenInLocal(token) {
+  localStorage.setItem("JWTToken", token);
+}
+
 loginRoute.post("/", async (req, res) => {
   const user = {
-    useremail: req.body.username,
+    useremail: req.body.useremail,
     password: req.body.password,
   };
   // Verify User Id and Password
@@ -17,18 +22,17 @@ loginRoute.post("/", async (req, res) => {
   console.log(req.body);
   const userCheck = await User.findOne({ email: user.useremail });
   console.log(userCheck);
-  if (userCheck !== undefined) {
+  if (userCheck !== undefined && userCheck !== null) {
     if (
       userCheck.email === user.useremail &&
       userCheck.password === user.password
     ) {
-      console.log("Successs");
       jwt.sign({ user }, SECRET_KEY, { expiresIn: "24h" }, (err, token) => {
         if (err) {
           console.log(err);
           res.sendStatus(400);
         } else {
-          res.status(200).json({
+          res.json({
             token,
           });
         }
