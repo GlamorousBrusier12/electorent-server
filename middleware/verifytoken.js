34262271c85db0eb5b4ece1b5a1bf23e82b40585
Ravindra Tholuchuru <tholuchururavindra@gmail.com>
2022-04-03 15:41:36 +0530

    Added login backend.

--- middleware/verifytoken.js
+++ middleware/verifytoken.js
@@ -1,6 +1,7 @@
 //Verify Token Function
-
-module.exports = function verifyToken(req, res, next) {
+import jwt from "jsonwebtoken";
+import { SECRET_KEY } from "../config/config.js";
+export default async function verifyToken(req, res, next) {
   //Get auth header value
   const bearerHeader = req.headers["authorization"];
 
@@ -10,8 +11,20 @@ module.exports = function verifyToken(req, res, next) {
     const bearer = bearerHeader.split(" ");
     const bearerToken = bearer[1];
     req.token = bearerToken;
-    next();
+    jwt.verify(req.token, SECRET_KEY, async (err, authData) => {
+      if (err) {
+        res.sendStatus(403);
+      } else {
+        const user = await User.findOne({ emailid: authData.user.emailid });
+        req.user = user;
+        res.json({
+          message: "Succesfully Verified.",
+          email: authData.user.emailid,
+          authData,
+        });
+      }
+    });
   } else {
     res.sendStatus(403);
   }
-};
+}
