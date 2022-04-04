4fd5f0b0fd5dfedc3179ecd9e1d914efd65535f9
Naveen <pandurebelsachin18@gmail.com>
2022-04-04 09:23:45 +0530

    user populate

--- models/Orders.js
+++ models/Orders.js
@@ -2,11 +2,11 @@ import mongoose from "mongoose";
 
 const OrderSchema = new mongoose.Schema({
   userId: {
-    type: mongoose.SchemaTypes.ObjectId,
+    type: mongoose.Schema.Types.ObjectId,
     ref: "User",
   },
   productId: {
-    type: mongoose.SchemaTypes.ObjectId,
+    type: mongoose.Schema.Types.ObjectId,
     ref: "Product",
   },
   qty: {
