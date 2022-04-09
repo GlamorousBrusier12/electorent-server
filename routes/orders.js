966fe06b715209377fbab6cca55b0df62b2c1694
Naveen <pandurebelsachin18@gmail.com>
2022-04-09 10:57:54 +0530

    add cloudinary config

--- routes/orders.js
+++ routes/orders.js
@@ -7,10 +7,11 @@ import {
   placeOrder,
   updateOrder,
 } from "../controllers/orders.js";
+import verifyToken from "../middleware/verifytoken.js";
 export const orderRouter = express.Router();
 
 // test route for api
-orderRouter.route("/").post(placeMultipleOrders).get(getAllOrders);
+orderRouter.route("/").post(verifyToken, placeMultipleOrders).get(getAllOrders);
 
 // get user related orders
 orderRouter.route("/:userId").get(getUserOrders);
