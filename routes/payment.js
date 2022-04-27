5353c3abc5635422094b4c63c36703a4c496bd65
Ravindra Tholuchuru <tholuchururavindra@gmail.com>
2022-04-27 07:23:08 +0530

    Added Stripe Payment Gateway

--- routes/payment.js
+++ routes/payment.js
@@ -3,33 +3,34 @@ import "dotenv/config";
 export const paymentRouter = express.Router();
 
 import Stripe from "stripe";
-const stripe = new Stripe(process.env.STRIPE_PRIVATE_KEY);
-const storeItems = new Map([
-  [1, { priceInCents: 10000, name: "Learn React Today" }],
-  [2, { priceInCents: 20000, name: "Learn CSS Today" }],
-]);
-
+const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
 paymentRouter.post("/", async (req, res) => {
-  const session = await stripe.checkout.sessions.create({
-    payment_method_types: ["card"],
-    line_items: [
-      {
-        // TODO: replace this with the `price` of the product you want to sell
-        // price: "{price_1KsH9USD3dBrJmKpYH25jbGP}",
-        price: "price_1KsImGSD3dBrJmKpp1hsz2Gi",
-        quantity: 1,
-      },
-      {
-        price: "price_1KsH9USD3dBrJmKpYH25jbGP",
-        quantity: 1,
-      },
-    ],
-    mode: "payment",
-    // success_url: `${YOUR_DOMAIN}?success=true`,
-    // cancel_url: `${YOUR_DOMAIN}?canceled=true`,
-    success_url: `http://localhost:3000/confirmation`,
-    cancel_url: `http://localhost:3000/`,
-  });
+  const { product, token } = req.body;
+  console.log("PRODUCT ", product);
+  console.log("PRICE ", product.price);
 
-  res.redirect(303, session.url);
+  return stripe.customers
+    .create({
+      email: token.email,
+      source: token.id,
+    })
+    .then((customer) => {
+      stripe.charges.create({
+        amount: product.price * 100,
+        currency: "usd",
+        customer: customer.id,
+        receipt_email: token.email,
+        description: `purchase of ${product.name}`,
+        shipping: {
+          name: token.card.name,
+          address: {
+            country: token.card.address_country,
+          },
+        },
+      });
+    })
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => console.log(err));
 });
